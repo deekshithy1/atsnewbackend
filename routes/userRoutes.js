742e73925fd941663bb17be8c481fb527042d248
sumithreddy6080 <sumithreddy.6080@gmail.com>
2025-07-10 15:23:28 +0530

    small change in user controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   createATSAdmin,
   createTechnician,
   getAllTechnicians,
+  getAllUserAtCenter,
 } from "../controllers/userController.js";
 
 import { protect, authorize } from "../middlewares/authMiddleware.js";
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.post('/ats-admin', protect, authorize('SUPER_ADMIN'), createATSAdmin);
 router.post('/technician', protect, authorize('ATS_ADMIN'), createTechnician);
-router.get('/technicians', protect, authorize('ATS_ADMIN'), getAllTechnicians);
+// router.get('/technicians', protect, authorize('ATS_ADMIN'), getAllTechnicians);
+router.get('/technicians', protect, authorize('ATS_ADMIN'), getAllUserAtCenter);
 
 export default router;
